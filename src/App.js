988ce3b73ef6b99d3a0dc4072640d9abec9cbc95
Dale Race <race.dale@gmail.com>
2018-01-23 20:03:04 -0600

    Update content, to trigger a circleCI build

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Dale Race</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          A portfolio site, made in ReactJS. Things are about to get fancy!
         </p>
       </div>
     );
