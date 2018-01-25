3e6ed9363fd60ac3565c720beacd62ad26fdd48f
Race, Dale <Dale.Race@mastercard.com>
2018-01-25 15:30:14 -0600

    Add css variables

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const CenteredDiv = styled.div`
 const App = () => (
   <CenteredDiv>
     <header>
-      <h1>Dale Race</h1>
+      <h1>First Last</h1>
     </header>
     <p>
       A portfolio site, made in ReactJS. Now on GitLab, things are about to get fancy!
