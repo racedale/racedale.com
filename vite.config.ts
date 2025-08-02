2e307c3ee90ea236c4ef14b7f3cb63a7f562af87
Dale <race.dale@gmail.com>
2025-08-01 18:10:42 -0600

    chore: update tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
